ee2190669446b36c0e0c2722c6e78bde9692396b
Frank van der Heijden <frank.boekanier@gmail.com>
2021-12-11 01:44:50 +0100

    v1.0.3 - Match all possible string substitutions

--- log4jfix-common/src/main/java/dev/frankheijden/log4jfix/common/PatternChecker.java
+++ log4jfix-common/src/main/java/dev/frankheijden/log4jfix/common/PatternChecker.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class PatternChecker {
 
-    private static final Pattern EXPLOIT_PATTERN = Pattern.compile("\\$\\{(jndi|log4j|sys|env|main|marker|java|base64|lower|upper|web|docker|kubernetes|spring|jvmrunargs|date|ctx)\\:.*\\}", Pattern.CASE_INSENSITIVE);
+    private static final Pattern EXPLOIT_PATTERN = Pattern.compile("\\$\\{.+\\}");
 
     private PatternChecker() {}
 
